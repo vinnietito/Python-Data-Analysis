b90d6c9dcc8fc63731f3b0df2149b0c518253c6a
vinnietito <vincentnjuguna69@gmail.com>
2024-12-04 19:09:30 +0300

    Commits

---
+++ DataFrames.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+# Creating DataFrame from a dictionary
+data = {'Name': ['Alice', 'Bob', 'Charlie'],
+        'Age': [25, 30, 35],
+        'City': ['New York', 'Los Angeles', 'Chicago']}
+
+df = pd.DataFrame(data)
+print(df)
